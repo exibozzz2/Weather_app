c58fdfd75f4d50104bcd699987894fafe4cf6109
exibozzz2 <masaradulovic96@gmail.com>
2025-01-29 03:41:33 +0100

    # Laravel 12
    # Create Seeder to:   (MultipleDaysSeeder)
    
    # Generate 5 different random dates
    # Generate 5 different random temperatures for each generated day for each city transfered from Cities Table !

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Laravel 12
-# Create Seeder to generate (count asked) random cities
+
+# Create Seeder to:   (MultipleDaysSeeder)
+
+# Generate 5 different random dates
+# Generate 5 different random temperatures for each generated day for each city transfered from Cities Table !
+
---
+++ database/seeders/MultipleDaysSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\CitiesModel;
+use App\Models\ForecastModel;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MultipleDaysSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $cities = CitiesModel::all();
+
+        foreach ($cities as $city) {
+            for($i = 0; $i < 5; $i++) {
+                ForecastModel::create([
+                   'city_id' => $city->id,
+                   'temperature' => rand(1, 20),
+                   'forecast_date' => Carbon::now()->addDays(rand(1, 30)),
+                ]);
+            }
+        }
+    }
+}
