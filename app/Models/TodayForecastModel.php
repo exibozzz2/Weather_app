a69cb1c784f9daffdbaf85687fc77786d0c7c1cc
exibozzz2 <masaradulovic96@gmail.com>
2025-01-31 02:04:51 +0100

    # Laravel #12 # Create Today Forecast Model (v.2) (protected - fillable )

--- app/Models/TodayForecastModel.php
+++ app/Models/TodayForecastModel.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class TodayForecastModel extends Model
 {
 
+    protected $table = "today_forecast";
+    protected $fillable = [
+      'city_id', 'temperature',
+    ];
+
 }
