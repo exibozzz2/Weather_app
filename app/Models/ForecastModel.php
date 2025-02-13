91f4add377ec3bf2371385ec58ac5112509393a8
exibozzz2 <masaradulovic96@gmail.com>
2025-02-13 13:43:33 +0100

    # Add condition in fillable foreacast model

--- app/Models/ForecastModel.php
+++ app/Models/ForecastModel.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class ForecastModel extends Model
 {
-    protected $table = "forecast";
+    protected $table = "forecasts";
 
     protected $fillable = [
-        'city_id', 'condition', 'temperature', 'forecast_date',
+        'city_id', 'temperature', 'forecast_date',
     ];
 }
