a71d02007f6da1b2f9e5448b7333bfafd335f202
exibozzz2 <masaradulovic96@gmail.com>
2025-02-20 16:59:01 +0100

    # Rename blade menageCities -> menageTemperature

--- resources/views/admin/menageTemperature.blade.php
+++ resources/views/admin/menageTemperature.blade.php
@@ -10,13 +10,13 @@
 
     <div class="d-flex justify-content-center bg-img-2">
 
-        <div class="col-10 d-flex justify-content-center align-items-center flex-column bg-card">
+        <div class="col-10 d-flex justify-content-center align-items-center flex-column bg-card gap-3">
 
-            <form class="col-12 d-flex justify-content-center m-5" method="POST" action="{{route("updateTemperature")}}" >
+            <form class="col-12 d-flex justify-content-center m-5 gap-3" method="POST" action="{{route("updateTemperature")}}" >
                 {{csrf_field()}}
 
-                <input class="" name="temperature" placeholder="Insert Temperature">
-                <select class="" name="city_id">
+                <input class="border-custom" name="temperature" placeholder="Insert Temperature">
+                <select class="border-custom" name="city_id">
                     @foreach(CitiesModel::all() as $city)
                         <option value="{{$city->id}}">{{$city->city}}</option>
                     @endforeach
@@ -26,7 +26,10 @@
 
 
             @foreach(CitiesModel::all() as $city)
-                <p>{{$city->city}} -> {{$city->current}} &#8451;</p>
+                <div class="col-2 bg-city border-custom d-flex justify-content-center align-items-center">
+                    <p><b>{{$city->city}}</b> -> <b>{{$city->current}}</b> &#8451;</p>
+                </div>
+
             @endforeach
 
 
