f310ecb8420fe9f28c57ae9b91593eaddda2ef85
exibozzz2 <masaradulovic96@gmail.com>
2025-02-24 00:58:54 +0100

    # Laravel # 15 # Create Helper which generate icon url based on condition

---
+++ app/Http/IconsHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http;
+
+class IconsHelper
+{
+    public static function getIconByCondition($weather)
+    {
+        $icons = [
+            "Sunny" => "sun.png",
+            "Sun" => "sun.png",
+            "Clear" => "sun.png",
+            "Fog" => "fog.png",
+            "Cloudy" => "cloudy.png",
+            "Cloud" => "cloud.png",
+            "Rain" => "rain.png",
+            "Rainy" => "rainy.png",
+            "Snow" => "snow.png",
+            "Wind" => "wind.png",
+            "Storm" => "storm.png"
+        ];
+
+        return isset($icons[$weather]) ? url('/images/' . $icons[$weather]) : null;
+    }
+}
--- resources/views/weatherCast.blade.php
+++ resources/views/weatherCast.blade.php
@@ -1,3 +1,6 @@
+@php use App\Http\IconsHelper; @endphp
+
+
 @extends("layout")
 
 @section("pageTitle")
@@ -6,6 +9,8 @@
 
 
 @section("content")
+
+
 
     <div class="d-flex flex-column col-12 justify-content-center align-items-center bg-img" style="height: 30vh">
 
@@ -22,51 +27,47 @@
         </div>
 
         @foreach($cities as $city)
-            <div class="card col-4 text-white bg-card mb-3 border-custom border-dark relative p-2 m-3"  data-tilt>
-                <div class="d-flex justify-content align-items-center flex-wrap">
-                    <h1 class="card-header">{{$city->city}}</h1>
-                    <h4 class="card-header">{{$city->current}}&#8451; / &#8457;</h4>
+
+            @php
+                $url = IconsHelper::getIconByCondition($city->weather);
+            @endphp
+
+                <div class="card col-4 text-white bg-card mb-3 border-custom border-dark relative p-2 m-3"  data-tilt>
+                    <div class="d-flex justify-content align-items-center flex-wrap">
+                        <h1 class="card-header">{{$city->city}}</h1>
+                        <h4 class="card-header">{{$city->current}}&#8451; / &#8457;</h4>
+                    </div>
+
+
+                    @if($url)
+                        <img class="icon col-2 absolute" src="{{ $url }}" alt="Weather Icon"/>
+                    @endif
+
+
+
+
+
+
+
+
+
+                    <div class="card-body ">
+                        <h3 class="card-title">{{$city->weather}}</h3>
+                        <p>{{$city->minimum}} / {{$city->maximum}} </p>
+                    </div>
+                    <div class="d-flex col-12 justify-content-between align-items-center flex-wrap text-white ">
+                        <a href="{{route("editCity", ['city' => $city->id])}}" class="btn bg-custom-2 border-custom border-white text-white">Edit</a>
+                        <a href="{{route("deleteCity", ['city' => $city->id])}}" class="btn bg-danger border-custom border-white text-white">Delete</a>
+                    </div>
                 </div>
 
-                @if($city->weather == "Sunny")
-                    <img class="icon col-2 absolute" src="{{url('/images/sun.png')}}" alt="Image"/>
-                @elseif($city->weather == "Sun")
-                    <img class="icon col-2 absolute" src="{{url('/images/sun.png')}}" alt="Image"/>
-                @elseif($city->weather == "Clear")
-                    <img class="icon col-2 absolute" src="{{url('/images/sun.png')}}" alt="Image"/>
-                @elseif($city->weather == "Fog")
-                    <img class="icon col-2 absolute" src="{{url('/images/fog.png')}}" alt="Image"/>
-                @elseif($city->weather == "Cloudy")
-                    <img class="icon col-2 absolute" src="{{url('/images/cloudy.png')}}" alt="Image"/>
-                @elseif($city->weather == "Cloud")
-                    <img class="icon col-2 absolute" src="{{url('/images/cloud.png')}}" alt="Image"/>
-                @elseif($city->weather == "Rain")
-                    <img class="icon col-2 absolute" src="{{url('/images/rain.png')}}" alt="Image"/>
-                @elseif($city->weather == "Rainy")
-                    <img class="icon col-2 absolute" src="{{url('/images/rainy.png')}}" alt="Image"/>
-                @elseif($city->weather == "Snow")
-                    <img class="icon col-2 absolute" src="{{url('/images/snow.png')}}" alt="Image"/>
-                @elseif($city->weather == "Wind")
-                    <img class="icon col-2 absolute" src="{{url('/images/wind.png')}}" alt="Image"/>
-                @elseif($city->weather == "Storm")
-                    <img class="icon col-2 absolute" src="{{url('/images/storm.png')}}" alt="Image"/>
-                @else
-
-                @endif
-
-
-                <div class="card-body ">
-                    <h3 class="card-title">{{$city->weather}}</h3>
-                    <p>{{$city->minimum}} / {{$city->maximum}} </p>
-                </div>
-                <div class="d-flex col-12 justify-content-between align-items-center flex-wrap text-white ">
-                    <a href="{{route("editCity", ['city' => $city->id])}}" class="btn bg-custom-2 border-custom border-white text-white">Edit</a>
-                    <a href="{{route("deleteCity", ['city' => $city->id])}}" class="btn bg-danger border-custom border-white text-white">Delete</a>
-                </div>
-            </div>
         @endforeach
 
+
     </div>
+
+
+
 
 
 
