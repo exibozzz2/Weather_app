60069619e019698c5fbaf3299b2c7715f9bd11cb
exibozzz2 <masaradulovic96@gmail.com>
2025-02-25 10:15:15 +0100

    # Laravel # 15 # Fix route names in navigation blade

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -2,13 +2,18 @@
 
 
 
-<div class="d-flex justify-content-around align-items-center bg-nav p-2 border-bottom-white">
-    <div class="col-4">
+<div class="d-flex justify-content-around align-items-center bg-nav p-2 border-bottom-white nav-rounded">
+    <div class="col-3">
         <a href="{{route("getAllCities")}}" class="text-decoration-none text-white"><h3>T O P | W E A T H E R</h3></a>
     </div>
         <a href="{{ route("getAllCities") }}" class="text-decoration-none text-white">All Cities</a>
+        <a href="{{ route("forecasts") }}" class="text-decoration-none text-white">Forecasts</a>
+        <a href="{{ route("todayForecast") }}" class="text-decoration-none text-white">Current</a>
         <a href="{{ route("addCity") }}" class="text-decoration-none text-white">Add City</a>
-        <a href="{{ route("forecasts") }}" class="text-decoration-none text-white">Next 5 days</a>
-        <a href="{{ route("currentForecast") }}" class="text-decoration-none text-white">Current</a>
+        <a href="{{ route("addForecast") }}" class="text-decoration-none text-white">Add Forecast</a>
+        <a href="{{ route("menageCities") }}" class="text-decoration-none text-white">Menage Cities</a>
+
+
+
 
 </div>
