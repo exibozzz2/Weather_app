26213fbc2f711ea7fc4f417e72eb08f03af2c28d
exibozzz2 <masaradulovic96@gmail.com>
2025-02-26 13:22:22 +0100

    # Laravel # 15 # Set possibility generator if condition is (Rain, Rainy or Snow) # Default value 0

--- database/seeders/MultipleDaysSeeder.php
+++ database/seeders/MultipleDaysSeeder.php
@@ -18,11 +18,19 @@ class MultipleDaysSeeder extends Seeder
 
         foreach (CitiesModel::all() as $city) {
             for($i = 0; $i < 5; $i++) {
+
+                $possibility = 0;
+                $condition = \App\Helpers\WeatherHelper::generateRandomCondition();
+                if($condition == "Rainy" || $condition == "Snow" || $condition == "Rain") {
+                    $possibility = rand(0, 100);
+                }
+
                 ForecastModel::create ([
                     'city_id' => $city->id,
-                    'temperature' => rand(1, 20),
+                    'temperature' => rand(-50, 38),
                     'forecast_date' => Carbon::now()->addDays(rand(1, 30)),
-
+                    'condition' => $condition,
+                    'possibility' => $possibility,
                 ]);
             }
         }
