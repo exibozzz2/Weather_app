d949a2bfabf41ac91048acb5a15d002a5de93d8d
exibozzz2 <masaradulovic96@gmail.com>
2025-03-05 16:01:28 +0100

    # Add casts variable to Forecast Model, convert date field

--- app/Models/ForecastModel.php
+++ app/Models/ForecastModel.php
@@ -9,8 +9,11 @@ class ForecastModel extends Model
     protected $table = "forecasts";
 
     protected $fillable = [
-        'city_id', 'temperature', 'forecast_date', 'condition',
+        'city_id', 'temperature', 'forecast_date', 'condition', 'possibility',
     ];
+
+    protected $casts = [ 'forecast_date' => 'datetime' ];
+
 
     const DAY = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" ];
 }
