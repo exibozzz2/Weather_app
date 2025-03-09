5ac2f4142d80ec29d3cada045e2332750ff419e4
exibozzz2 <masaradulovic96@gmail.com>
2025-03-09 22:47:03 +0100

    # Laravel #16 # Add Href permalink to all search results # Add Icon to searched results

--- resources/views/searchResults.blade.php
+++ resources/views/searchResults.blade.php
@@ -9,11 +9,20 @@
                 <h1>Search Results &#8675; &#8675; &#8675;</h1>
             </div>
 
+
+
             @foreach($cities as $city)
-                <a>
-                    <div class="d-flex justify-content-center align-items-center flex-column border border-white border-custom m-2">
+                <a href="{{route("singleCityForecast", ['city' => $city->city])}}">
+                    @php
+                        $url = \App\Helpers\IconsHelper::getIconByCondition($city->weather);
+                    @endphp
+
+                    <div class="d-flex justify-content-center align-items-center flex-column border border-white border-custom m-2 relative">
                         <h2 class="text-white m-2">{{$city->city}}</h2>
-                        <p>{{$city->current}}</p>
+                        <p>{{$city->current}}&#8451;</p>
+                        @if($url)
+                            <img class="search-weather-icon col-3  absolute" src="{{ $url }}" alt="Weather Icon"/>
+                        @endif
                     </div>
                 </a>
             @endforeach
