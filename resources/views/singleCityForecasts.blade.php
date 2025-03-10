05032b4ab4b336b0229f803e44f5619c1894327e
exibozzz2 <masaradulovic96@gmail.com>
2025-03-10 14:09:50 +0100

    # Single City Forecasts # Implement Color Helper by temperature in Single City Forecasts blade # Change date format in Single City blade via casts   • A full textual representation of a month   • Day count from 01 to 31   • A full textual representation of a day # Little front changes

--- resources/views/singleCityForecasts.blade.php
+++ resources/views/singleCityForecasts.blade.php
@@ -10,11 +10,17 @@
 
 
         <div class="col-8 d-flex justify-content-center align-items-center h-50 bg-card flex-wrap border-custom gap-5" data-tilt>
-
+            <h1 class="col-12 text-center text-white">Next 5 days</h1>
                 @foreach($singleCityForecasts as $singleCity)
-                    <div class="h-25 d-flex flex-column justify-content-center align-items-center gap-5 hover-card">
-                        <h5>{{\App\Models\ForecastModel::DAY[rand(0,6)]}} {{$singleCity->forecast_date}} -> </h5>
-                        <h5>{{$singleCity->temperature}} &#8451;</h5>
+
+                    @php
+                    $color = App\Helpers\ColorHelper::getColorByTemperature($singleCity->temperature);
+                    @endphp
+
+                    <div class="h-25 col-3 d-flex justify-content-center align-items-center gap-5 hover-card border-custom">
+
+                        <h5>{{$singleCity->forecast_date->setTimezone('Europe/Belgrade')->format('F d l')}}</h5>
+                        <h5 style="color: {{$color}}">{{$singleCity->temperature}} &#8451;</h5>
                     </div>
 
                 @endforeach
