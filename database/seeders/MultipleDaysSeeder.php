e57d2c85467332ed9746d09882f2c017de7892ab
exibozzz2 <masaradulovic96@gmail.com>
2025-03-10 12:21:40 +0100

    # Laravel #16 # Add Forecasts days chronologically # Carbon::now()->addDays($i)

--- database/seeders/MultipleDaysSeeder.php
+++ database/seeders/MultipleDaysSeeder.php
@@ -16,22 +16,54 @@ class MultipleDaysSeeder extends Seeder
     public function run(): void
     {
 
+
+
         foreach (CitiesModel::all() as $city) {
+
+            $lastTemperature = 0;
+
             for($i = 0; $i < 5; $i++) {
 
-                $possibility = 0;
+                $possibility = rand(0, 1);
                 $condition = \App\Helpers\WeatherHelper::generateRandomCondition();
                 if($condition == "Rainy" || $condition == "Snow" || $condition == "Rain") {
                     $possibility = rand(0, 100);
                 }
 
+                $temperature = 0;
+                if($lastTemperature !== null) {
+                    $minTemperature = $lastTemperature-5;
+                    $maxTemperature = $lastTemperature+5;
+                    $temperature = rand($minTemperature, $maxTemperature);
+                }
+
+                else {
+                    switch($condition) {
+                        case "Sunny":
+                            $temperature = rand(-20, 38);
+                            break;
+                        case "Cloudy":
+                            $temperature = rand(-20, 15);
+                            break;
+                        case "Rainy":
+                            $temperature = rand(-10, 38);
+                            break;
+                        case "Snow":#
+                            $temperature = rand(-20, 1);
+                            break;
+                    }
+                }
+
                 ForecastModel::create ([
                     'city_id' => $city->id,
-                    'temperature' => rand(-50, 38),
-                    'forecast_date' => Carbon::now()->addDays(rand(1, 30)),
+                    'temperature' => $temperature,
+                    'forecast_date' => Carbon::now()->addDays($i),
                     'condition' => $condition,
                     'possibility' => $possibility,
                 ]);
+
+
+                $lastTemperature = $temperature;
             }
         }
     }
