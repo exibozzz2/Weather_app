87df0767651e7d73c3c6b38f4c10573192992642
exibozzz2 <masaradulovic96@gmail.com>
2025-03-11 16:44:26 +0100

    # Laravel #17 # Create OneToOne relation between User and UserFavourites # user_id -> foreign key # id      -> local key

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,4 +47,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function favourites() {
+        return $this->hasOne(UserFavouritesModel::class, 'user_id', 'id' );
+    }
 }
