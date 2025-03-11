9e010acb14832b16aabbdf6484d661474e49d7b6
exibozzz2 <masaradulovic96@gmail.com>
2025-03-11 10:16:45 +0100

    # Laravel #17 # Create user favourites model

---
+++ app/Models/UserFavouritesModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserFavouritesModel extends Model
+{
+    protected $table = 'user_favourites';
+
+    protected $fillable = [
+        'user_id', 'city_id',
+    ];
+}
