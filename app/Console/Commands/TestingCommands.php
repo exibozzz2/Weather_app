4b4008a8b05c03e4774cd4b16662f3f31b50eaed
exibozzz2 <masaradulovic96@gmail.com>
2025-04-16 10:19:50 +0200

    # Laravel #19 # Define typed argument, and compare it via eloquent query in Cities model #dd()

--- app/Console/Commands/TestingCommands.php
+++ app/Console/Commands/TestingCommands.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\CitiesModel;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 
@@ -27,16 +28,21 @@ class TestingCommands extends Command
     public function handle()
     {
 
+        $databaseCity = CitiesModel::where(['city' => $this->argument('city')])->first();
+        dd($databaseCity->city, $databaseCity->id);
 
-        $response = Http::get(env("WEATHER_API_URL")."v1/current.json", [
+        $response = Http::get(env("WEATHER_API_URL")."v1/forecast.json", [
             'key' => env("WEATHER_API_KEY"),
             'q' => $this->argument('city'),
             'aqi' => 'no',
             'lang' => 'ru',
+            'days' => 1,
         ]);
 
 
         $jsonResponse = $response->json();
+
+
 
         if(isset($jsonResponse['error'])) {
             $this->output->error("This city doesn't exists.");
