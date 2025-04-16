7c7f59bd8d9b6a542cb7a3e787350c636dc9c7ff
exibozzz2 <masaradulovic96@gmail.com>
2025-04-16 16:03:44 +0200

    # Laravel #19 # Homework # If city doesn't exists create new one + import forecast for today # If city exists just create forecast for today # Import forecast data from api call and define them as variable # If forecast for today already exists skip api call -> output comment # If forecast for today doesn't exist create new one -> output comment

--- app/Console/Commands/TestingCommands.php
+++ app/Console/Commands/TestingCommands.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\CitiesModel;
+use App\Models\ForecastModel;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 
@@ -29,27 +30,56 @@ class TestingCommands extends Command
     {
 
         $databaseCity = CitiesModel::where(['city' => $this->argument('city')])->first();
-        dd($databaseCity->city, $databaseCity->id);
 
         $response = Http::get(env("WEATHER_API_URL")."v1/forecast.json", [
             'key' => env("WEATHER_API_KEY"),
             'q' => $this->argument('city'),
             'aqi' => 'no',
-            'lang' => 'ru',
-            'days' => 1,
+            'lang' => 'en',
         ]);
-
 
         $jsonResponse = $response->json();
 
-
+        // if city doesn't exists
+        if($databaseCity === null) {
+            CitiesModel::create([
+                'city' => $this->argument('city'),
+                'weather' => $jsonResponse['current']['condition']['text'],
+            ]);
+            $this->output->comment("Successfully created new city");
+        }
 
         if(isset($jsonResponse['error'])) {
             $this->output->error("This city doesn't exists.");
             return;
         }
 
-        dd($jsonResponse['current']['condition']['text']);
+          $cityId = $databaseCity->id;
+          $temperature = $jsonResponse['current']['temp_c'];
+          $forecastDate = $jsonResponse['forecast']['forecastday']['0']['date'];
+          $condition = $jsonResponse['current']['condition']['text'];
+          $possibility = $jsonResponse['forecast']['forecastday']['0']['day']['daily_will_it_rain'];
+
+        $forecast = [
+            'city_id' => $cityId,
+            'temperature' => $temperature,
+            'forecast_date' => $forecastDate,
+            'condition' => $condition,
+            'possibility' => $possibility,
+        ];
+
+        if($databaseCity->todayForecast !== null) {
+
+            $this->output->comment("Forecast for today already updated.");
+            return;
+        }
+
+        ForecastModel::create($forecast);
+        $this->output->comment("Successfully created new forecast");
+
+
+
+
 
     }
 }
