2b79109e58464f6b6ca9854228132e4f3f749cd1
exibozzz2 <masaradulovic96@gmail.com>
2025-04-24 14:18:24 +0200

    # Laravel #20 # Change Icon Helper with match instead conditional statements

--- app/Helpers/IconsHelper.php
+++ app/Helpers/IconsHelper.php
@@ -5,7 +5,25 @@ namespace App\Helpers;
 class IconsHelper
 {
 
-    const ICONS    = [
+    const ICONS = [
+        "Sunny" => "sun.png",
+        "Sun" => "sun.png",
+        "Clear" => "sun.png",
+        "Fog" => "fog.png",
+        "Cloudy" => "cloudy.png",
+        "Cloud" => "cloud.png",
+        "Rain" => "rain.png",
+        "Rainy" => "rainy.png",
+        "Snow" => "snow.png",
+        "Wind" => "wind.png",
+        "Storm" => "storm.png",
+        "Moderate or heavy rain with thunder" => "rain.png",
+    ];
+
+    public static function getIconByCondition($weather)
+    {
+
+        $icon = match ($weather) {
             "Sunny" => "sun.png",
             "Sun" => "sun.png",
             "Clear" => "sun.png",
@@ -16,21 +34,24 @@ class IconsHelper
             "Rainy" => "rainy.png",
             "Snow" => "snow.png",
             "Wind" => "wind.png",
-            "Storm" => "storm.png"
-        ];
+            "Storm" => "storm.png",
+            default => "sun.png",
+        };
+        return $icon;
 
-    public static function getIconByCondition($weather)
-    {
-        if(in_array($weather, self::ICONS)) {
-
-            return self::ICONS[$weather];
-        }
-        else if($weather === null) {
-            return "sun.png";
-        }
-        return "sun.png";
     }
 }
 
 //        return isset($icons[$weather]) ? url('/images/' . $icons[$weather]) : null;
 //        $url = self::WEATHER_ICONS[$type] //$url = self::WEATHER_ICONS[$type]
+
+//
+//if(in_array($weather, self::ICONS)) {
+//
+//    return self::ICONS[$weather];
+//}
+//else if($weather === null) {
+//    return "sun.png";
+//}
+//return "sun.png";
+//}
