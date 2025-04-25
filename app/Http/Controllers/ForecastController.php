33454430f2488b8626a9e93da2a69f7c740bd704
exibozzz2 <masaradulovic96@gmail.com>
2025-04-25 03:33:30 +0200

    # Laravel #20 # Import Astro data info in Single City Blade # Add api call in to forecast controller # Upgrade single city blade (card view) # Copy custom css for card view from previous project

--- app/Http/Controllers/ForecastController.php
+++ app/Http/Controllers/ForecastController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\CitiesModel;
 use App\Models\ForecastModel;
+use App\Services\WeatherService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ForecastController extends Controller
 {
@@ -17,7 +19,24 @@ class ForecastController extends Controller
 
     public function singleCity(CitiesModel $city) {
 
+        $response = Http::get(env("WEATHER_API_URL")."v1/forecast.json", [
+            'key' => env("WEATHER_API_KEY"),
+            'q' => $city->city,
+            'aqi' => 'no',
+            'lang' => 'en',
+        ]);
+
+        $jsonResponse = $response->json();
+
+        $cityName = $city->city;
+
+        $currentForecast = $jsonResponse['forecast']['forecastday'][0]['day'];
+        $sunrise = $jsonResponse['forecast']['forecastday'][0]['astro']['sunrise'];
+        $sunset = $jsonResponse['forecast']['forecastday'][0]['astro']['sunset'];
+        $minTemperature = $currentForecast['mintemp_c'];
+        $maxTemperature = $currentForecast['maxtemp_c'];
+
         $singleCityForecasts = ForecastModel::where(['city_id' => $city->id])->get();
-        return view('singleCityForecasts', compact('singleCityForecasts'));
+        return view('singleCityForecasts', compact('singleCityForecasts', 'sunrise', 'sunset', 'minTemperature', 'maxTemperature', 'cityName'));
     }
 }
--- resources/views/singleCityForecasts.blade.php
+++ resources/views/singleCityForecasts.blade.php
@@ -6,28 +6,68 @@
 
 @section("content")
 
-    <div class="d-flex justify-content-center align-items-center bg-img">
+    <div class="d-flex flex-wrap justify-content-center align-items-center bg-img-2">
 
-
-        <div class="col-8 d-flex justify-content-center align-items-center h-50 bg-card flex-wrap border-custom gap-5" data-tilt>
-            <h1 class="col-12 text-center text-white">Next 5 days</h1>
                 @foreach($singleCityForecasts as $singleCity)
 
                     @php
-                    $color = App\Helpers\ColorHelper::getColorByTemperature($singleCity->temperature);
+                    $url = App\Helpers\IconsHelper::getIconByCondition($singleCity->condition);
                     @endphp
 
-                    <div class="h-25 col-3 d-flex justify-content-center align-items-center gap-5 hover-card border-custom">
-
-                        <h5>{{$singleCity->forecast_date->setTimezone('Europe/Belgrade')->format('F d l')}}</h5>
-                        <h5 style="color: {{$color}}">{{$singleCity->temperature}} &#8451;</h5>
+                <div class="card-1 rgb col-8 m-3" data-tilt >
+                    <div class="card-image d-flex justify-content-around align-items-center text-secondary">
+                        <h1 class="text-secondary">{{$singleCity->forecast_date->setTimezone('Europe/Belgrade')->format('l')}}</h5></h1>
+                        <img src="/Images/{{$url}}" class="col-3 h-70 card-image border-custom" alt="coverimg">
                     </div>
+
+                    <div class="col-12 d-flex justify-content-around align-items-center">
+                        <div class="card-text col-4">
+                            <h2>{{$singleCity->city}}</h2>
+                            <p>• Sunrise: {{$sunrise}}</p>
+                            <p>• Sunset: {{$sunset}}</p>
+                            <p>• Min temperature: {{$minTemperature}} &#8451;</p>
+                            <p>• Max temperature: {{$maxTemperature}} &#8451;</p>
+                        </div>
+                        <div class="card-text d-flex justify-content-around align-items-center col-4 flex-wrap ">
+                            <h3 class="">{{$singleCity->condition}}</h3>
+                        </div>
+
+
+                    </div>
+
+
+                    <div class="card-stats">
+                        <div class="stat">
+                            <div class="type">City ID</div>
+                            <div class="value">{{$singleCity->city_id}}<sup> </sup></div>
+                        </div>
+
+                        <div class="d-flex justify-content-center align-items-center bg-custom-3 border">
+                            <a href="/" class="btn text-white">{{$singleCity->forecast_date->setTimezone('Europe/Belgrade')->format('d,F')}}</a>
+                        </div>
+                        <div class="stat">
+                            <div class="value">{{$singleCity->possibility}}%</div>
+                            <div class="type">Will it Rain?</div>
+                        </div>
+                    </div>
+                </div>
+{{--                    <p>{{$sunrise}}</p>--}}
+{{--                    <p>{{$sunset}}</p>--}}
+
+{{--                    @php--}}
+{{--                    $color = App\Helpers\ColorHelper::getColorByTemperature($singleCity->temperature);--}}
+{{--                    @endphp--}}
+
+{{--                    <div class="h-25 col-3 d-flex justify-content-center align-items-center gap-5 hover-card border-custom">--}}
+
+{{--                        <h5>{{$singleCity->forecast_date->setTimezone('Europe/Belgrade')->format('F d l')}}</h5>--}}
+{{--                        <h5 style="color: {{$color}}">{{$singleCity->temperature}} &#8451;</h5>--}}
+{{--                    </div>--}}
 
                 @endforeach
 
         </div>
 
-    </div>
 
 
 
